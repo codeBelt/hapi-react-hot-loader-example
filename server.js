245990f2fcb179951add35c2114951769324bf98
codeBelt <code@codebelt.com>
2017-07-24 23:53:34 -0500

    add notifier and clean up server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const Hapi = require('hapi');
 const fs = require('fs');
+const Webpack = require('webpack');
 
 const env = process.env.NODE_ENV;
 const isProduction = (env === 'production');
@@ -7,20 +8,46 @@ const isProduction = (env === 'production');
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
 
-// if (isProduction === false) {
+if (isProduction === false) {
     const WebpackPlugin = require('hapi-webpack-plugin');
+    const compiler = new Webpack(require('./webpack.config.js'));
+
+    compiler.plugin('done', (stats) => {
+        setTimeout(() => {
+            console.log(`\n\nServer running at: ${server.info.protocol}://localhost:${server.info.port}\n`)
+        }, 0);
+
+        const pkg = require('./package.json');
+        const notifier = require('node-notifier');
+        const time = ((stats.endTime - stats.startTime) / 1000).toFixed(2);
+
+        notifier.notify({
+            title: pkg.name,
+            message: `WebPack is done!\n${stats.compilation.errors.length} errors in ${time}s`,
+            timeout: 1, // Takes precedence over wait if both are defined.
+        });
+    });
+
+    const assets = {
+        // webpack-dev-middleware options
+        // See https://github.com/webpack/webpack-dev-middleware
+    };
+
+    const hot = {
+        // webpack-hot-middleware options
+        // See https://github.com/glenjamin/webpack-hot-middleware
+    };
 
     server.register({
         register: WebpackPlugin,
-        options: './webpack.config.js'
+        options: {compiler, assets, hot}
     }, error => {
         if (error) {
             return console.error(error);
         }
 
-        server.start(() => console.log(`Server running at: ${server.info.uri}`));
     });
-// }
+}
 
 server.route({
     method: 'GET',
@@ -38,3 +65,5 @@ server.route({
         });
     }
 });
+
+server.start();
