ef6b8914dfe58da72421ed7e52d182f45f2fd815
codeBelt <code@codebelt.com>
2017-08-07 23:36:53 -0500

    add redux form

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,8 +2,10 @@ import {combineReducers} from 'redux';
 import UserReducer from './user/UserReducer';
 import LoadingReducer from './loading/LoadingReducer';
 import MetaReducer from './meta/MetaReducer';
+import {reducer as formReducer} from 'redux-form';
 
 const rootReducer = combineReducers({
+    form: formReducer,
     userReducer: UserReducer.reduce,
     loadingReducer: LoadingReducer.reduce,
     metaReducer: MetaReducer.reduce,
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {connect} from 'react-redux';
 import MetaAction from '../store/meta/MetaAction';
-
+import {Field, FormProps, reduxForm} from 'redux-form';
 
 const mapStateToProps = (state) => ({});
 
@@ -77,4 +77,176 @@ class Contact extends React.Component {
 
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Contact);
+Contact = connect(mapStateToProps, mapDispatchToProps)(Contact);
+
+export default reduxForm({
+    form: 'example' // a unique name for this form
+})(Contact);
+
+
+
+// import * as React from 'react';
+// import {Field, FormProps, reduxForm} from 'redux-form';
+// import INewsletter from '../../interfaces/reducers/INewsletter';
+//
+// interface INewsletterFormProps extends FormProps<INewsletter, void, void> {
+//     actions: any;
+// }
+//
+// export const NEWSLETTER_FORM_NAME: string = 'newsletterForm';
+//
+// class NewsletterForm extends React.Component<INewsletterFormProps, void> {
+//
+//     public render(): JSX.Element {
+//         const { handleSubmit } = this.props;
+//
+//         return (
+//             <form
+//                 className="inputForm u-vr_x6_bplarge"
+//                 onSubmit={handleSubmit((formData: INewsletter) => this._onFormSubmit(formData))}
+//             >
+//                 <Field
+//                     name="firstName"
+//                     label="Fist Name"
+//                     placeholder="Fist Name"
+//                     component={this._renderInputField}
+//                 />
+//                 <Field
+//                     name="lastName"
+//                     label="Last Name"
+//                     placeholder="Last Name"
+//                     component={this._renderInputField}
+//                 />
+//                 <Field
+//                     name="email"
+//                     label="Email"
+//                     placeholder="Email"
+//                     component={this._renderInputField}
+//                 />
+//                 <Field
+//                     name="phone"
+//                     label="Phone Number"
+//                     placeholder="Phone Number"
+//                     component={this._renderInputField}
+//                 />
+//                 <Field
+//                     name="message"
+//                     label="Message"
+//                     placeholder="Message"
+//                     component={this._renderTextArea}
+//                 />
+//                 <div className="inputForm-item">
+//                     <Field
+//                         label="I am a Nerdery employee"
+//                         name="isEmployee"
+//                         type="checkbox"
+//                         component={this._renderCheckbox}
+//                     />
+//                     <Field
+//                         label="Notify Me for Special Offers!"
+//                         name="allowSpecialOffers"
+//                         type="checkbox"
+//                         component={this._renderCheckbox}
+//                     />
+//                 </div>
+//                 <div className="inputForm-item">
+//                     <input
+//                         className="btn btn_dark"
+//                         type="submit"
+//                         value="Sign Up"
+//                     />
+//                 </div>
+//             </form>
+//         );
+//     }
+//
+//     private _renderInputField(field: any): any {
+//         const { meta: { touched, error } } = field;
+//         const className: string = `inputForm-text ${touched && error ? 'inputForm-warning' : ''}`;
+//
+//         return (
+//             <div className="inputForm-item inputForm-item_half">
+//                 <label
+//                     className="u-isVisuallyHidden"
+//                     htmlFor={field.input.name}
+//                 >
+//                     {field.label}
+//                 </label>
+//                 <input
+//                     {...field.input}
+//                     type="text"
+//                     className={className}
+//                     placeholder={field.placeholder}
+//                 />
+//             </div>
+//         );
+//     }
+//
+//     private _renderCheckbox(field: any): any {
+//         return (
+//             <div className="inputForm-item-checkGroup">
+//                 <input
+//                     {...field.input}
+//                     className="u-isVisuallyHidden"
+//                     type="checkbox"
+//                 />
+//                 <label htmlFor={field.input.name}>{'Notify Me for Special Offers!'}</label>
+//             </div>
+//         );
+//     }
+//
+//     private _renderTextArea(field: any): any {
+//         const { meta: { touched, error } } = field;
+//         const className: string = `inputForm-area ${touched && error ? 'inputForm-warning' : ''}`;
+//
+//         return (
+//             <div className="inputForm-item">
+//                 <label
+//                     className="u-isVisuallyHidden"
+//                     htmlFor={field.input.name}
+//                 >
+//                     {field.label}
+//                 </label>
+//                 <textarea
+//                     {...field.input}
+//                     className={className}
+//                     placeholder={field.placeholder}
+//                 />
+//             </div>
+//         );
+//     }
+//
+//     private _onFormSubmit(formData: INewsletter): void {
+//         this.props.actions.signUpForNewsletter(formData);
+//     }
+//
+// }
+//
+// export default reduxForm({
+//     form: NEWSLETTER_FORM_NAME,
+//     validate: (formData: INewsletter) => {
+//         const errors: any = {};
+//
+//         if (!formData.firstName) {
+//             errors.firstName = 'Required';
+//         }
+//
+//         if (!formData.lastName) {
+//             errors.lastName = 'Required';
+//         }
+//
+//         if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(formData.email)) {
+//             errors.email = 'Invalid email address';
+//         }
+//
+//         if (!formData.phone) {
+//             errors.phone = 'Required';
+//         }
+//
+//         if (!formData.message) {
+//             errors.message = 'Required';
+//         }
+//
+//         return errors;
+//     },
+// })(NewsletterForm);
