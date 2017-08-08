a5047e885998522f501b1657752e5ea60839aca0
codeBelt <code@codebelt.com>
2017-08-07 23:11:40 -0500

    rename

--- src/server.js
+++ src/server.js
@@ -4,13 +4,13 @@ import * as inert from 'inert';
 import ServerManager from './server-side/ServerManager';
 import AssetsController from './server-side/controllers/AssetsController';
 import ReactController from './server-side/controllers/ReactController';
-import WebpackPlugin from "./server-side/plugin/WebpackPlugin";
+import HapiWebpackHotPlugin from "./server-side/plugin/HapiWebpackHotPlugin";
 
 const manager = new ServerManager();
 manager.registerPlugin(inert);
 
 if (manager.isProduction === false) {
-    new WebpackPlugin(manager.server);
+    new HapiWebpackHotPlugin(manager.server);
 }
 
 manager.registerController(new AssetsController());
