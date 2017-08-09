24879f762d3a73cd0bd41ffe892c4c2d2343483a
rsavian <rsavian@nerdery.com>
2017-08-09 16:45:37 -0500

    update form

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -32,7 +32,7 @@ class Contact extends React.Component {
                             name="name"
                             type="text"
                             label="Name"
-                            placeholder="Enter name"
+                            placeholder=""
                             component={this._renderInputField}
                         />
                     </div>
@@ -41,7 +41,7 @@ class Contact extends React.Component {
                             name="email"
                             type="email"
                             label="Email"
-                            placeholder="Enter email"
+                            placeholder="example@example.com"
                             component={this._renderInputField}
                         />
                         <small id="emailHelp" className="form-text text-muted">{'We\'ll never share your email with anyone else.'}</small>
@@ -57,8 +57,12 @@ class Contact extends React.Component {
                         </select>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="messageTextArea">{'Message'}</label>
-                        <textarea className="form-control" id="messageTextArea" rows="3"></textarea>
+                        <Field
+                            name="message"
+                            label="Message"
+                            placeholder=""
+                            component={this._renderTextArea}
+                        />
                     </div>
                     <fieldset className="form-group">
                         <legend>{'Radio buttons'}</legend>
@@ -82,10 +86,12 @@ class Contact extends React.Component {
                         </div>
                     </fieldset>
                     <div className="form-check">
-                        <label className="form-check-label">
-                            <input type="checkbox" className="form-check-input" />
-                            {'Did you star my repo?'}
-                        </label>
+                        <Field
+                            label="Did you star my repo?"
+                            name="starred"
+                            type="checkbox"
+                            component={this._renderCheckbox}
+                        />
                     </div>
                     <button type="submit" className="btn btn-primary">{'Submit'}</button>
                 </form>
@@ -100,9 +106,8 @@ class Contact extends React.Component {
 
     _renderInputField(field) {
         const {meta: {touched, error}} = field;
-        console.log(`touched, error`, touched, error);
         const className = `small text-danger ${touched && error ? '' : 'd-none'}`;
-console.log(`className`, className);
+
         return (
             <span>
                 <label htmlFor={field.input.name}>
@@ -121,35 +126,37 @@ console.log(`className`, className);
 
     _renderCheckbox(field) {
         return (
-            <div className="inputForm-item-checkGroup">
+            <label
+                className="form-check-label"
+                htmlFor={field.input.name}
+            >
                 <input
                     {...field.input}
-                    className="u-isVisuallyHidden"
+                    className="form-check-input"
                     type="checkbox"
                 />
-                <label htmlFor={field.input.name}>{'Notify Me for Special Offers!'}</label>
-            </div>
+                {field.label}
+            </label>
         );
     }
 
     _renderTextArea(field) {
         const { meta: { touched, error } } = field;
-        const className = `inputForm-area ${touched && error ? 'inputForm-warning' : ''}`;
+        const className = `small text-danger ${touched && error ? '' : 'd-none'}`;
 
         return (
-            <div className="inputForm-item">
-                <label
-                    className="u-isVisuallyHidden"
-                    htmlFor={field.input.name}
-                >
-                    {field.label}
+            <span>
+                <label htmlFor={field.input.name}>
+                    {field.label} <span className={className}>{error}</span>
                 </label>
                 <textarea
                     {...field.input}
-                    className={className}
+                    className="form-control"
                     placeholder={field.placeholder}
-                />
-            </div>
+                    rows="3"
+                >
+                </textarea>
+            </span>
         );
     }
 
@@ -170,182 +177,10 @@ export default reduxForm({
             errors.email = 'Invalid email address';
         }
 
-        // if (!formData.phone) {
-        //     errors.phone = 'Required';
-        // }
-
-        // if (!formData.message) {
-        //     errors.message = 'Required';
-        // }
+        if (!formData.message) {
+            errors.message = 'Required';
+        }
 
         return errors;
     },
 })(Contact);
-
-
-
-// import * as React from 'react';
-// import {Field, FormProps, reduxForm} from 'redux-form';
-// import INewsletter from '../../interfaces/reducers/INewsletter';
-//
-// interface INewsletterFormProps extends FormProps<INewsletter, void, void> {
-//     actions;
-// }
-//
-// export const NEWSLETTER_FORM_NAME = 'newsletterForm';
-//
-// class NewsletterForm extends React.Component<INewsletterFormProps, void> {
-//
-//     public render(): JSX.Element {
-//         const { handleSubmit } = this.props;
-//
-//         return (
-//             <form
-//                 className="inputForm u-vr_x6_bplarge"
-//                 onSubmit={handleSubmit((formData: INewsletter) => this._onFormSubmit(formData))}
-//             >
-//                 <Field
-//                     name="firstName"
-//                     label="Fist Name"
-//                     placeholder="Fist Name"
-//                     component={this._renderInputField}
-//                 />
-//                 <Field
-//                     name="lastName"
-//                     label="Last Name"
-//                     placeholder="Last Name"
-//                     component={this._renderInputField}
-//                 />
-//                 <Field
-//                     name="email"
-//                     label="Email"
-//                     placeholder="Email"
-//                     component={this._renderInputField}
-//                 />
-//                 <Field
-//                     name="phone"
-//                     label="Phone Number"
-//                     placeholder="Phone Number"
-//                     component={this._renderInputField}
-//                 />
-//                 <Field
-//                     name="message"
-//                     label="Message"
-//                     placeholder="Message"
-//                     component={this._renderTextArea}
-//                 />
-//                 <div className="inputForm-item">
-//                     <Field
-//                         label="I am a Nerdery employee"
-//                         name="isEmployee"
-//                         type="checkbox"
-//                         component={this._renderCheckbox}
-//                     />
-//                     <Field
-//                         label="Notify Me for Special Offers!"
-//                         name="allowSpecialOffers"
-//                         type="checkbox"
-//                         component={this._renderCheckbox}
-//                     />
-//                 </div>
-//                 <div className="inputForm-item">
-//                     <input
-//                         className="btn btn_dark"
-//                         type="submit"
-//                         value="Sign Up"
-//                     />
-//                 </div>
-//             </form>
-//         );
-//     }
-//
-//     _renderInputField(field) {
-//         const { meta: { touched, error } } = field;
-//         const className = `inputForm-text ${touched && error ? 'inputForm-warning' : ''}`;
-//
-//         return (
-//             <div className="inputForm-item inputForm-item_half">
-//                 <label
-//                     className="u-isVisuallyHidden"
-//                     htmlFor={field.input.name}
-//                 >
-//                     {field.label}
-//                 </label>
-//                 <input
-//                     {...field.input}
-//                     type="text"
-//                     className={className}
-//                     placeholder={field.placeholder}
-//                 />
-//             </div>
-//         );
-//     }
-//
-//     _renderCheckbox(field) {
-//         return (
-//             <div className="inputForm-item-checkGroup">
-//                 <input
-//                     {...field.input}
-//                     className="u-isVisuallyHidden"
-//                     type="checkbox"
-//                 />
-//                 <label htmlFor={field.input.name}>{'Notify Me for Special Offers!'}</label>
-//             </div>
-//         );
-//     }
-//
-//     _renderTextArea(field) {
-//         const { meta: { touched, error } } = field;
-//         const className = `inputForm-area ${touched && error ? 'inputForm-warning' : ''}`;
-//
-//         return (
-//             <div className="inputForm-item">
-//                 <label
-//                     className="u-isVisuallyHidden"
-//                     htmlFor={field.input.name}
-//                 >
-//                     {field.label}
-//                 </label>
-//                 <textarea
-//                     {...field.input}
-//                     className={className}
-//                     placeholder={field.placeholder}
-//                 />
-//             </div>
-//         );
-//     }
-//
-//     _onFormSubmit(formData: INewsletter){
-//         this.props.actions.signUpForNewsletter(formData);
-//     }
-//
-// }
-//
-// export default reduxForm({
-//     form: NEWSLETTER_FORM_NAME,
-//     validate: (formData: INewsletter) => {
-//         const errors = {};
-//
-//         if (!formData.firstName) {
-//             errors.firstName = 'Required';
-//         }
-//
-//         if (!formData.lastName) {
-//             errors.lastName = 'Required';
-//         }
-//
-//         if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(formData.email)) {
-//             errors.email = 'Invalid email address';
-//         }
-//
-//         if (!formData.phone) {
-//             errors.phone = 'Required';
-//         }
-//
-//         if (!formData.message) {
-//             errors.message = 'Required';
-//         }
-//
-//         return errors;
-//     },
-// })(NewsletterForm);
