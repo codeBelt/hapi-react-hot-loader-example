56d834a9b2dddd486f8ab846d51039b01f2f7239
codebelt <code@codebelt.com>
2017-08-12 13:02:30 -0500

    rename server-side to server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import 'fetch-everywhere';
 import * as inert from 'inert';
 
-import ServerManager from './server-side/ServerManager';
-import AssetsController from './server-side/controllers/AssetsController';
-import ReactController from './server-side/controllers/ReactController';
-import HapiWebpackHotPlugin from './server-side/plugin/HapiWebpackHotPlugin';
+import ServerManager from './server/ServerManager';
+import AssetsController from './server/controllers/AssetsController';
+import ReactController from './server/controllers/ReactController';
+import HapiWebpackHotPlugin from './server/plugin/HapiWebpackHotPlugin';
 
 const manager = new ServerManager();
 manager.registerPlugin(inert);
