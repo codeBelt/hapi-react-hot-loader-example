7d69cb08e1fc2667c3570b70209c2a837e967a0d
codebelt <code@codebelt.com>
2017-08-13 20:05:27 -0500

    fix spelling

--- src/services/ServerService.js
+++ src/services/ServerService.js
@@ -1,16 +1,16 @@
 class ServerService {
 
     static createLocationObject(request) {
-        const protocal = request.headers['x-forwarded-proto'] || request.connection.info.protocol;
+        const protocol = request.headers['x-forwarded-proto'] || request.connection.info.protocol;
 
         return {
             ...request.url,
             host: request.info.host,
             hostname: request.info.host.split(':')[0],
-            href: `${protocal}://${request.info.host}${request.url.path}`,
-            origin: `${protocal}://${request.info.host}`,
+            href: `${protocol}://${request.info.host}${request.url.path}`,
+            origin: `${protocol}://${request.info.host}`,
             pathname: request.url.path.split('?')[0],
-            protocal: `${protocal}:`,
+            protocol: `${protocol}:`,
         };
     }
 
